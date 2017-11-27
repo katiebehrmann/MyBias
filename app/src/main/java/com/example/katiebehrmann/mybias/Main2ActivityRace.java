f618fd2043f41e468723b51f7dcddcdc015af06f
katiebehrmann <kbehrmann@tc.columbia.edu>
2017-11-27 15:11:30 -0500

    Initial push

---
+++ app/src/main/java/com/example/katiebehrmann/mybias/Main2ActivityRace.java
@@ -0,0 +1,13 @@
+package com.example.katiebehrmann.mybias;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Main2ActivityRace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2_race);
+    }
+}
